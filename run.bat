7002f204f2e9e6ff8aae8b9d001e2bc625b6bab2
Alexo <alexsuciultc200@gmail.com>
2025-03-26 10:56:46 +0200

    run.bat

--- run.bat
+++ run.bat
@@ -2,5 +2,10 @@
 echo Starting backend...
 start cmd /k "cd backend && node index"
 timeout /t 5
+
 echo Starting frontend...
 start cmd /k "cd frontend && npm run start"
+timeout /t 5
+
+echo Starting Trivia's frontend on port 3001...
+start cmd /k "cd trivia && set PORT=3001 && npm run start"
